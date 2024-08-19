2aceea55ea8bff212b83a0b91a1e1ae83fce4caf
MartinYeung5 <luckylucky2010201@yahoo.com.hk>
2024-08-20 01:03:29 +0800

    20240820build10

--- packages/chain/src/runtime/index.ts
+++ packages/chain/src/runtime/index.ts
@@ -2,15 +2,18 @@ import { Balance, VanillaRuntimeModules } from "@proto-kit/library";
 import { ModulesConfig } from "@proto-kit/common";
 
 import { Balances } from "./modules/balances";
+import { GuestBook } from "./modules/guest-book";
 
 export const modules = VanillaRuntimeModules.with({
   Balances,
+  GuestBook,
 });
 
 export const config: ModulesConfig<typeof modules> = {
   Balances: {
     totalSupply: Balance.from(2_000),
   },
+  GuestBook: {},
 };
 
 export default {
